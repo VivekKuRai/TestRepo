82ff2dcebb5943e7fd054b11989850669a7099e2
Vivek Rai <vivekrai.uptu@gmail.com>
2021-06-22 12:11:12 +0530

    uploaded the project to git repository

---
+++ TestRepo/src/test/java/com/home/work/services/HelloWorldRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.work.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
