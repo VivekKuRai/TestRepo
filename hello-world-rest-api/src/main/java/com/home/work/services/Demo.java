bbcad164f4c23bc4e85b839577b5be61251f05a0
Vivek Rai <vivekrai.uptu@gmail.com>
2023-04-07 18:45:56 +0530

    Demo File added

---
+++ hello-world-rest-api/src/main/java/com/home/work/services/Demo.java
@@ -0,0 +1,10 @@
+package com.home.work.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+    public static void main(String[] args) {
+        List<String> list =new ArrayList();
+    }
+}
